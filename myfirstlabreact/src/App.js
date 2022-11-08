8de852b1a706f15b0513f6c1e3631e078c6dcaf2
Mario Cezar Priotto <mariopriotto@gmail.com>
2022-11-08 01:47:21 -0300

    Passando objeto (imagem) como parametro

--- myfirstlabreact/src/App.js
+++ myfirstlabreact/src/App.js
@@ -2,10 +2,16 @@ import ironhacklogoxs from './images/ironhack-logo-xs.png';
 import menutopxs from './images/menu-top-xs.png';
 import ComponenteColuna from './Components/Componentecoluna/ComponenteColuna';
 
+import  ico1 from './images/icon1.png';
+import  ico2 from './images/icon2.png';
+import  ico3 from './images/icon3.png';
+import  ico4 from './images/icon4.png';
+
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
 
     <div className="AppframeTop">
@@ -15,41 +21,23 @@ function App() {
       </div>
       <p className="ApptitleFrame">Say hello to</p>
       <p className="ApptitleFrame">ReactJS</p>
-
       <p className="AppdescritionFrame">You will learn how to use</p>
       <p className="AppdescritionFrame">the most popular frontend library,</p>
       <p className="AppdescritionFrame">and become a super Ninja developer.</p>
-
       <button className="AppAwesome">Awesome!</button>
     </div>
 
     <div className="AppframeButton">
       <div className="ApplineTwo">
-        <ComponenteColuna imagem="0" titulo="Declarative" textual="React makes it painless to create interactive UIs."/>
-        <ComponenteColuna imagem="1" titulo="Components"  textual="Build encapsulated components that manage their state."/>
-        <ComponenteColuna imagem="2" titulo="Single-Way"  textual="A set of immutable values are passed to the component's"/>
-        <ComponenteColuna imagem="3" titulo="JSX"         textual="Statically-typed, designed to run on modern browsers."/>
+        <ComponenteColuna imagem={JSON.stringify(ico1)} titulo="Declarative" textual="React makes it painless to create interactive UIs."/>
+        <ComponenteColuna imagem={JSON.stringify(ico2)} titulo="Components"  textual="Build encapsulated components that manage their state."/>
+        <ComponenteColuna imagem={JSON.stringify(ico3)} titulo="Single-Way"  textual="A set of immutable values are passed to the component's"/>
+        <ComponenteColuna imagem={JSON.stringify(ico4)} titulo="JSX"         textual="Statically-typed, designed to run on modern browsers."/>
       </div>
     </div>
 
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-
     </div>
+
   );
 }
 
--- myfirstlabreact/src/Components/Componentecoluna/ComponenteColuna.js
+++ myfirstlabreact/src/Components/Componentecoluna/ComponenteColuna.js
@@ -1,16 +1,17 @@
-import  ico1 from '../../images/icon1.png';
-import  ico2 from '../../images/icon2.png';
-import  ico3 from '../../images/icon3.png';
-import  ico4 from '../../images/icon4.png';
+// import  ico1 from '../../images/icon1.png';
+// import  ico2 from '../../images/icon2.png';
+// import  ico3 from '../../images/icon3.png';
+// import  ico4 from '../../images/icon4.png';
 
 import './ComponenteColuna.css';
 
-let matrizIcones = [ico1, ico2, ico3, ico4];
+// let matrizIcones = [ico1, ico2, ico3, ico4];
 
 function ComponenteColuna (props) {
     return (
         <article className="colunaArticle" >
-            <img className="colArtImg" src={matrizIcones[props.imagem]}/>
+            <img className="colArtImg" src={JSON.parse(props.imagem)}/>
+            {/* <img className="colArtImg" src={matrizIcones[props.imagem]}/> */}
             <h1 className="colArtTit">{props.titulo}</h1>
             <p className="colArtTex">{props.textual}</p>            
         </article>
